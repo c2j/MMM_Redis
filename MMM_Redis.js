e3f884397490e245dbcb1e7c3929b911de0d89a0
ChenJianjun <c2j@chenkaideMBP.lan>
2020-01-05 10:31:18 +0800

    Redis queue

--- MMM_Redis.js
+++ MMM_Redis.js
@@ -18,6 +18,37 @@ Module.register("MMM_Redis",{
 	getDom: function() {
 		var wrapper = document.createElement("div");
 		wrapper.innerHTML = this.config.text;
+
+		var client = require("redis").createClient(6379, "localhost");
+	
+		sys.puts("waiting for messages...");
+		client.on(
+			"error",
+			function(err){
+				console.log("err"+err);
+				}
+		);
+		client.subscribe("face_message");
+		client.on('subscribe',
+			function(channel,count){
+				console.log("channel:" + channel + ", count:"+count);
+				}
+		);
+		client.on('message',
+			function(channel,message){
+				console.log("channel:" + channel + ", msg:"+message);
+				wrapper.innerHTML = message;
+				}
+		);
+		client.on('unsubscribe',
+			function(channel,count){
+				console.log("channel:" + channel + ", count:"+count);
+	
+	/* ————————————————
+	版权声明：本文为CSDN博主「cuipingxu51111」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+	原文链接：https://blog.csdn.net/cuipingxu51111/article/details/40183925
+	*/
+		
 		return wrapper;
 	}
 });
