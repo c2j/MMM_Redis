697b350c456afc79fd466ed916cff1085e9959a7
ChenJianjun <c2j@chenkaideMBP.lan>
2020-01-11 00:56:27 +0800

    Redis queue

--- node_helper.js
+++ node_helper.js
@@ -10,44 +10,45 @@ module.exports = NodeHelper.create({
 
 	start: function () {
         console.log("Redis Help start...");
+        var key = "face_message"; //this.config.channel;
+
+        var redisSubClient = redis.createClient(6379, "localhost");
+        
+        // 客户端连接redis成功后执行回调
+        redisSubClient.on("ready", () => {
+            // 订阅消息
+            redisSubClient.subscribe(key);
+            console.info("订阅成功。。。");
+        });
+
+        redisSubClient.on("error", error => {
+            console.info("Redis Error " + error);
+        });
+        // 监听订阅成功事件
+        redisSubClient.on('subscribe', (channel, count)=>{
+            console.info("client subscribed to " + channel + "," + count + "total subscriptions");
+        });
+        // 收到消息后执行回调
+        redisSubClient.on('message', (channel, message)=>{
+            Log.info(`收到 ${channel} 频道的消息： ${message}`);
+            self.sendSocketNotification("STATUS", message);
+        });
+        // 监听取消订阅事件
+        redisSubClient.on("unsubscribe", (channel, count) => {
+            console.info("client unsubscribed from" + channel + ", " + count + " total subscriptions")
+        });
 	},
 
 	
-
-	socketNotificationReceived: function (notification, payload) {
+    
+	socketNotificationReceived: function(notification, payload) {
         var self = this;
         console.log("Redis Help socketNotificationReceived...");
         Log.info("Redis Help socketNotificationReceived...");
         console.info(notification+" helper "+payload);
 		if (notification === "CONFIG") {
             this.config = payload;
-            var key = this.config.channel;
-
-			var redisSubClient = redis.createClient(6379, "localhost");
             
-            // 客户端连接redis成功后执行回调
-            redisSubClient.on("ready", () => {
-                // 订阅消息
-                redisSubClient.subscribe(key);
-                console.info("订阅成功。。。");
-            });
-
-            redisSubClient.on("error", error => {
-                console.info("Redis Error " + error);
-            });
-            // 监听订阅成功事件
-            redisSubClient.on('subscribe', (channel, count)=>{
-                console.info("client subscribed to " + channel + "," + count + "total subscriptions");
-            });
-            // 收到消息后执行回调
-            redisSubClient.on('message', (channel, message)=>{
-                Log.info(`收到 ${channel} 频道的消息： ${message}`);
-                self.sendSocketNotification("STATUS", message);
-            });
-            // 监听取消订阅事件
-            redisSubClient.on("unsubscribe", (channel, count) => {
-                console.info("client unsubscribed from" + channel + ", " + count + " total subscriptions")
-            });
 		} 
 	}
 
